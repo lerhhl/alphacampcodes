467ce0c23b2596b9cb0e08a49ccaace45d92a052
lerhhl <lerhhl@gmail.com>
2017-05-08 09:28:58 +0800

    Shorten the swapping_puzzle

---
+++ swapping_puzzle/swapping_puzzle_r2.rb
@@ -0,0 +1,120 @@
+def swapping_puzzle (num)
+	blank_index = 0
+	final_blank_index = 0
+	init_stat = []
+	cur_stat = []
+	x_no_move = false
+	o_no_move = false
+	count_move = 0
+
+	#Create initial puzzle table
+	if num.even? == false
+		blank_index = ((num + 1) / 2) - 1
+	else
+		blank_index = (num / 2) - 1
+	end
+	for i in 0..(blank_index - 1) do
+		init_stat = init_stat.push("O")
+	end
+	init_stat = init_stat.push("-")
+	for i in (blank_index + 1)..(num - 1) do
+		init_stat = init_stat.push("X")
+	end
+	cur_stat = init_stat
+	print_table(cur_stat)
+
+	#Create final puzzle table
+	final_stat = init_stat.reverse
+	final_blank_index = blank_index
+
+	# Main program starts here
+	while (cur_stat != final_stat) do
+		x_no_move, o_no_move = false, false
+
+	result_blk = Proc.new do |stat, type|
+		if type == "X"
+			print_table(cur_stat)
+			blank_index = stat.index("-")
+			count_move += 1	
+			x_no_move = false
+		elsif type == "O"
+			print_table(cur_stat)
+			blank_index = stat.index("-")
+			count_move += 1	
+			o_no_move = false
+		end	
+	end
+
+	#Check any next movable "O"
+		while (o_no_move == false) do
+	#"O" can move forwards?
+			if (blank_index == cur_stat.length - 1) && (cur_stat[blank_index - 1] == "O")
+				cur_stat[blank_index], cur_stat[blank_index - 1] = cur_stat[blank_index - 1], cur_stat[blank_index]
+				result_blk.call(cur_stat, "O")	
+			elsif (blank_index - 1 >= 0) && (cur_stat[blank_index - 1] == "O") && (cur_stat[blank_index + 1] != cur_stat[blank_index - 1])
+				cur_stat[blank_index], cur_stat[blank_index - 1] = cur_stat[blank_index - 1], cur_stat[blank_index]
+				result_blk.call(cur_stat, "O")
+			elsif (blank_index > final_blank_index) && (cur_stat[blank_index - 1] == "O")
+				cur_stat[blank_index], cur_stat[blank_index - 1] = cur_stat[blank_index - 1], cur_stat[blank_index]
+				result_blk.call(cur_stat, "O")
+	#"O" can jump forwards?				
+			elsif (blank_index == cur_stat.length - 1) && (cur_stat[blank_index - 2] == "O")
+				cur_stat[blank_index], cur_stat[blank_index - 2] = cur_stat[blank_index - 2], cur_stat[blank_index]
+				result_blk.call(cur_stat, "O")
+			elsif (blank_index - 2 >= 0) && (cur_stat[blank_index - 2] == "O") && (cur_stat[blank_index + 1] != cur_stat[blank_index - 2])
+				cur_stat[blank_index], cur_stat[blank_index - 2] = cur_stat[blank_index - 2], cur_stat[blank_index]
+				result_blk.call(cur_stat, "O")
+			elsif (blank_index > final_blank_index) && (cur_stat[blank_index - 2] == "O")
+				cur_stat[blank_index], cur_stat[blank_index - 2] = cur_stat[blank_index - 2], cur_stat[blank_index]
+				result_blk.call(cur_stat, "O")				
+			else
+				o_no_move = true
+			end
+		end
+
+	#Check any next movable "X"
+		while (x_no_move == false) do
+
+	#"X" can move forwards?
+			if (blank_index == 0) && (cur_stat[blank_index + 1] == "X")
+				cur_stat[blank_index], cur_stat[blank_index + 1] = cur_stat[blank_index + 1], cur_stat[blank_index]
+				result_blk.call(cur_stat, "X")		
+			elsif (blank_index + 1 >= 0) && (cur_stat[blank_index + 1] == "X") && (cur_stat[blank_index - 1] != cur_stat[blank_index + 1])
+				cur_stat[blank_index], cur_stat[blank_index + 1] = cur_stat[blank_index + 1], cur_stat[blank_index]
+				result_blk.call(cur_stat, "X")				
+			elsif (blank_index < final_blank_index) && (cur_stat[blank_index + 1] == "X")
+				cur_stat[blank_index], cur_stat[blank_index + 1] = cur_stat[blank_index + 1], cur_stat[blank_index]
+				result_blk.call(cur_stat, "X")					
+	#"X" can jump forwards?				
+			elsif (blank_index == 0) && (cur_stat[blank_index + 2] == "X")
+				cur_stat[blank_index], cur_stat[blank_index + 2] = cur_stat[blank_index + 2], cur_stat[blank_index]
+				result_blk.call(cur_stat, "X")				
+			elsif (blank_index + 2 >= 0) && (cur_stat[blank_index + 2] == "X") && (cur_stat[blank_index - 1] != cur_stat[blank_index + 2])
+				cur_stat[blank_index], cur_stat[blank_index + 2] = cur_stat[blank_index + 2], cur_stat[blank_index]
+				result_blk.call(cur_stat, "X")	
+			elsif (blank_index < final_blank_index) && (cur_stat[blank_index + 2] == "X")
+				cur_stat[blank_index], cur_stat[blank_index + 2] = cur_stat[blank_index + 2], cur_stat[blank_index]
+				result_blk.call(cur_stat, "X")				
+			else
+				x_no_move = true
+			end
+		end
+	end
+	print "\n Total number of moves #{count_move} \n"
+end	
+
+def print_table (stat)
+	print ("\n")
+	print (stat).join(",")
+	print ("\n")
+	print ("-"*stat.length*2)
+	print("\n")
+end
+
+input = 0
+while (input % 2 == 0) || (input < 5)
+	print ("Please enter an odd number starting from 5. (eg. 5 or 7 or 9)")
+	input = gets.chomp.to_i
+end
+
+swapping_puzzle(input)

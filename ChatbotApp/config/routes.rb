7e03597d17990d5616fed97c44fb64939826439f
lerhhl <lerhhl@gmail.com>
2017-05-22 22:50:20 +0800

    Finish user signup

--- ChatbotApp/config/routes.rb
+++ ChatbotApp/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/new'
+#  get 'users/new'
 
   root 'static_pages#home'
   get  '/help', to: 'static_pages#help'
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   get  '/signup',  to: 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  resources :users
 end
